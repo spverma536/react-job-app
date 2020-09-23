65c5f823a89099b936e0c6e8c36e2d5b58798fc5
spverma536 <spverma536@gmail.com>
2020-09-24 02:02:08 +0530

    Updates

---
+++ precache-manifest.d520b7bddb8db8c50664e92fdfc5f8d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e23d5a61b9df0f536bc14b7443e0fbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f222295ecb167a33123",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47b36ef7aee6a942c396",
+    "url": "/static/js/2.7b86ff87.chunk.js"
+  },
+  {
+    "revision": "c5f452a49db7e2387d7422ce54985a99",
+    "url": "/static/js/2.7b86ff87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f222295ecb167a33123",
+    "url": "/static/js/main.8e632be3.chunk.js"
+  },
+  {
+    "revision": "951886f89f36abbe0731",
+    "url": "/static/js/runtime-main.c5d2d69f.js"
+  }
+]);
