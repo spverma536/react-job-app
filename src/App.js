e45bd13d9ab13645f4c990323261bd4ef2226612
spverma536 <spverma536@gmail.com>
2020-09-24 01:54:48 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import { Form } from 'reactstrap';
+import './App.css';
+import useFetchJobs from './useFetchJobs'
+import { Container } from 'react-bootstrap'
+import Job from './Job'
+import JobsPagination from './JobsPagination'
+import SearchForm from './SearchForm'
+
+function App() {
+  const [params, setParams] = useState([])
+  const [page, setPage] = useState(1)
+  const {jobs, loading, error, hasNextPage} = useFetchJobs(params, page)
+
+  function handleParamsChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return {...prevParams, [param]: value}
+    })
+  }
+
+  return (
+    <Container className="my-4">
+      <h1 className="mb-4">Latest Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamsChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && <h1>loading...</h1>}
+      {error && <h1>Error. Try refreshing</h1>}
+      {jobs.map (job => {
+        return <Job key={job.id} job={job} />
+      })}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+    </Container>
+  )
+}
+
+export default App;
